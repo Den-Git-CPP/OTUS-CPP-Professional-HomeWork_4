a17e25516de7341eb524cb0a87fdf1dbcf8be21f
Den-Git-CPP <madim-neodart@rambler.ru>
2022-05-15 01:09:22 +0300

    входные данные

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+ #include "pint_ip.h"
+
+ /**
+ * @brief Entry point
+ * Execution of the program starts here.
+ * @return Exit status
+ */
+int main( )
+{
+ std::cout<<"Work Print_IP"<<std::endl; 
+ return 0; 
+}
